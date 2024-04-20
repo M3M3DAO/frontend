0be67b67d045eae8736d306a910dc28c3afde108
solidity12 <volibear1001@gmail.com>
2024-04-21 00:12:12 +0900

    feat: Update accountChanged function

--- src/MetaMask.js
+++ src/MetaMask.js
@@ -9,33 +9,38 @@ const MetaMask = () => {
 
     const connectWallet = () => {
         if (window.ethereum) {
-            window.ethereum.request({method: 'eth_requestAccounts'})
-            .then(result => {
-                accountChanged([result[0]]);
-            }) 
+            window.ethereum.request({ method: 'eth_requestAccounts' })
+                .then(result => {
+                    accountChanged([result[0]]);
+                })
         } else {
             setErrorMessage('Install MetaMask!')
         }
     }
 
     const accountChanged = (accountName) => {
-        setDefaultAccount(accountName);
+        setDefaultAccount(`${accountName[0].substring(
+            0,
+            8
+        )}...${accountName[0].substring(accountName[0].length - 6)}`);
     }
 
-    return(
+    return (
         <div className="item">
             {!defaultAccount ? (
                 errorMessage ? (
                     // TODO: Fix to make a popup with error 
                     // if metamask isn't installed
-                       <h3>{errorMessage}</h3>
+                    <h3>{errorMessage}</h3>
                 ) : (
                     <button className="btn" onClick={connectWallet}>
                         Connect Wallet
                     </button>
-                    )
+                )
             ) : (
-                <h3>Address: {defaultAccount}</h3>
+                <button className="btn">
+                    {defaultAccount}
+                </button>
             )}
         </div>
     )
